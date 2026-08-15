1ca62b189af9999152f0d66cdc41722f5c5cc46d
KK <126642469+frogkingg@users.noreply.github.com>
2026-08-15 15:51:20 +0800

    feat: publish Notion knowledge bundle

---
+++ src/index-store/index.ts
@@ -0,0 +1,7 @@
+/** Local SQLite index persistence and retrieval APIs. */
+
+export * from './store.ts'
+export { INDEX_APPLICATION_ID, INDEX_SCHEMA_VERSION, IndexStoreError } from './schema.ts'
+export type { IndexStoreErrorCode } from './schema.ts'
+export * from './tokenize.ts'
+export type * from './types.ts'
